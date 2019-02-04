a09b55a025b95c38879409826867990883b8d1fb
Colin Murtaugh <cmurtaugh@gmail.com>
2019-02-03 22:05:39 -0500

    MIDDLEWARE_CLASSES --> MIDDLEWARE

--- project_name/settings/local.py
+++ project_name/settings/local.py
@@ -8,7 +8,7 @@ SECRET_KEY = '{{ secret_key }}'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 INSTALLED_APPS.extend(['debug_toolbar'])
-MIDDLEWARE_CLASSES.extend(['debug_toolbar.middleware.DebugToolbarMiddleware'])
+MIDDLEWARE.extend(['debug_toolbar.middleware.DebugToolbarMiddleware'])
 
 # For Django Debug Toolbar:
 INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
